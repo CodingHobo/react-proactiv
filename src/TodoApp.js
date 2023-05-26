d4abd291f8581cbeb30ef47139358769c96ec77d
Tanya Shylock <tourtanya@gmail.com>
2023-05-26 11:16:43 -0600

    edit todo.app

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,6 +3,7 @@ import { v4 as uuid } from "uuid";
 
 import TopTodo from "./TopTodo";
 import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm";
 
 /** App for managing a todo list.
  *
@@ -15,57 +16,62 @@ import EditableTodoList from "./EditableTodoList";
  * App -> TodoApp -> { TodoForm, EditableTodoList }
  */
 
-function TodoApp({ initialTodos=[] }) {
+function TodoApp({ initialTodos = [] }) {
   const [todos, setTodos] = useState(initialTodos);
-
 
   /** add a new todo to list */
   function create(newTodo) {
-    let newTodo = { ...newTodo, id: uuid() };
-    setTodos(todos => [...todos, newTodo]);
+    newTodo = { ...newTodo, id: uuid() };
+    setTodos((todos) => [...todos, newTodo]);
   }
 
   /** update a todo with updatedTodo */
   function update(updatedTodo) {
-    setTodos(todos =>
-      todos.map(todo =>
-        todo.id === updatedTodo.id
-        ? updatedTodo
-        : todo
-      )
+    setTodos((todos) =>
+      todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
     );
   }
 
   /** delete a todo by id */
   function remove(id) {
-    setTodos(todos => todos.filter(todo => todo.id !== id));
-
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }
 
+  /** New todo form saved. */
+  function handleSave(formData) {
+    create(formData);
+  }
+
+  let hasTodos = todos.length > 0;
+
   return (
-      <main className="TodoApp">
-        <div className="row">
+    <main className="TodoApp">
+      <div className="row">
+        <div className="col-md-6">
+          {hasTodos && <EditableTodoList />}
+          {!hasTodos && <span className="text-muted">You have no todos.</span>}
+        </div>
 
-          <div className="col-md-6">
-            <EditableTodoList /> OR
-            <span className="text-muted">You have no todos.</span>
-          </div>
-
-          <div className="col-md-6">
-            (if no top todo, omit this whole section)
+        <div className="col-md-6">
+          {hasTodos && (
             <section className="mb-4">
               <h3>Top Todo</h3>
               <TopTodo />
             </section>
+          )}
 
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
-              FIXME
-            </section>
-          </div>
-
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm initialFormData={{
+          title: "",
+          description: "",
+          priority: ""
+        }}
+        handleSave={handleSave}/>
+          </section>
         </div>
-      </main>
+      </div>
+    </main>
   );
 }
 
